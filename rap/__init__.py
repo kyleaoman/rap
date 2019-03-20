aa2e62b000e3c8cd29cd2bca727687a23d8cef81
K.A. Oman <koman@astro.rug.nl>
2019-03-20 12:15:00 +0100

    Fix import errors.

--- rap/__init__.py
+++ rap/__init__.py
@@ -1,2 +1,2 @@
-from _rap import Rap
-from _demo import demo
+from ._rap import Rap
+from ._demo import demo
--- rap/_demo.py
+++ rap/_demo.py
@@ -10,7 +10,7 @@ def demo():
     btrue = 0
     truths = (atrue, btrue)
     model.xi = np.linspace(0, 1, 10)
-    model.yi = (atrue * model.xi + btrue) + np.random.rand(10) * .1
+    model.yi = (atrue * model.xi + btrue) + (np.random.rand(10) - .5) * .1
 
     R = Rap(model)
     guess = [np.array([1, 0]) + np.random.rand(2) * .01 for i in range(10)]
@@ -22,9 +22,9 @@ def demo():
         pp.plot(model.xi, model.yi, 'ok')
         abest, bbest = R.results['theta_ml']
         ys = abest * model.xi + bbest
-        pp.plot(model.xi, ys, '-b')
+        pp.plot(model.xi, ys, ls='solid', color='slategray')
         yt = atrue * model.xi + btrue
-        pp.plot(model.xi, yt, '-r')
+        pp.plot(model.xi, yt, ls='solid', color='red')
         pp.xlabel('x')
         pp.ylabel('y')
         pp.savefig(pdffile, format='pdf')
